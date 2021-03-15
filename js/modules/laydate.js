443a34e92a202e478dbfb4369e3fe00eef784a19
STAYORLEAVE <282505081@qq.com>
2021-03-15 10:56:35 +0800

    refactor: laydatejs

--- js/modules/laydate.js
+++ js/modules/laydate.js
@@ -2,42 +2,66 @@
   /**
    * readme https: //www.layui.com/laydate/
    */
-  Drupal.behaviors.date = {
+  Drupal.behaviors.dyniva_laydate = {
     _dates: {},
+    first: true,
     attach: function (context) {
       var that = this;
 
+      if(that.first) {
+        that.init(context);
+
+        that.first = false;
+      }
+    },
+    init: function (context) {  
+      var that = this;
       var $date = $('[data-toggle="date"]', context);
 
       $date.each(function (index) {
-        var $ele = $(this);
-        $ele.addClass('dy-date-' + index);
-
-        var defaults = {
-          elem: '.dy-date-' + index
-        };
-        var options = null;
-
-        // 参数为JSON字符串
-        try {
-          options = JSON.parse($ele.attr('data-options'))
-        } catch (error) {
-          options = {}
-        }
-
-        var optionMark = getMarks($ele);
-        var marks = optionMark ? {
-          mark: optionMark
-        } : {};
-
-        var configs = $.extend(defaults, options, marks)
-
-        that._dates[index] = laydate.render(configs);
+        that.render(this, index);
       })
+    },
+    render: function (ele, key) {
+      var $ele = $(ele);
+      var that = this;
 
-      function getMarks(ele) {
-        return ele.data('mark')
+      $ele.addClass('dy-date-' + key);
+
+      var defaults = {
+        elem: '.dy-date-' + key,
+        ready: function (date) {
+          $ele.trigger('laydate.ready', { date: date });
+        },
+        change: function (value, date, endDate) {
+          $ele.trigger('laydate.change', { value: value, date: date, endDate: endDate })
+        },
+        done: function (value, date, endDate) {
+          $ele.trigger('laydate.done', { value: value, date: date, endDate: endDate })
+        }
+      };
+      
+      var options = null;
+
+      // 参数为JSON字符串
+      try {
+        options = JSON.parse($ele.attr('data-options'))
+      } catch (error) {
+        options = {}
       }
+
+      var optionMark = that.getMarks($ele);
+
+      var marks = optionMark ? {
+        mark: optionMark
+      } : {};
+
+      var configs = $.extend(defaults, options, marks)
+
+      that._dates[key] = laydate.render(configs);
+    },
+    getMarks: function ($ele) {
+      return $ele.data('mark')
     }
   }
 
