f1642546682b191751070525990f52a2ac20cbe0
STAYORLEAVE <282505081@qq.com>
2021-03-15 10:56:45 +0800

    refactor: slimscrolljs

--- js/commons/slimscroll.js
+++ js/commons/slimscroll.js
@@ -1,22 +1,45 @@
 (function ($) {
-  $(document).ready(function () {
-    $("[data-toggle=slimScroll]").each(function () {
+  Drupal.behaviors.slimScroll = {
+    first: true,
+    attach: function (context, settings) {
       var self = this;
-      
-      if (self.scrollHeight > self.clientHeight) {
-        var option = JSON.parse($(self).attr("data-slimScroll") || "{}");
-        if (option && typeof option === "object")
+
+      if (self.first) {
+        self.init(context);
+
+        self.first = false;
+      }
+    },
+    init: function (context) {
+      var self = this;
+
+      $("[data-toggle=slimScroll]", context).each(function () {
+        self.scroll(this);
+      });
+    },
+    scroll: function (scrollWrapper) {
+      var $scrollWrapper = $(scrollWrapper);
+      // scolled -> return
+      if($scrollWrapper.hasClass('slimscrolled')) return;
+
+      if (scrollWrapper.scrollHeight > scrollWrapper.clientHeight) {
+        var option = JSON.parse($scrollWrapper.attr("data-slimScroll") || "{}");
+
+        if (option && typeof option === "object") {
           $.extend(option, {
-            height: self.clientHeight + "px",
+            height: scrollWrapper.clientHeight + "px",
             railVisible: true,
           });
-        $(self).slimScroll(option);
-        
-        var hideScrollDefault = $(self).attr('data-hide-scroll');
-        if(hideScrollDefault) {
-          $(self).parent().find('.slimScrollBar').hide();
+        }
+
+        $scrollWrapper.slimScroll(option).trigger('slimscrolled').addClass('slimscrolled');
+
+        var hideScrollDefault = $scrollWrapper.attr('data-hide-scroll');
+
+        if (hideScrollDefault) {
+          $scrollWrapper.parent().find('.slimScrollBar').hide();
         }
       }
-    });
-  });
+    }
+  };
 })(jQuery);
